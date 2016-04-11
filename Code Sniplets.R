56f788c36d55dd73bb03c103aea30a150078482a
jbakerr <Baker@Bakers-MacBook-Pro.local>
2016-04-11 08:43:56 -0400

    First Commit

---
+++ Code Sniplets.R
@@ -0,0 +1,21 @@
+#clear workspace
+rm(list = ls())
+
+
+#Find Primes
+is.prime <- function(x){
+  if(x == 1){
+    flag <- FALSE
+    }
+  if(x == 2 | x == 1){
+    
+  flag <- FALSE
+  }
+  else { 
+    
+    flag <- !any(x %% 2:(floor(sqrt(x))) == 0)
+  }
+  return(flag)
+}
+  
+
---
+++ Notes.R
@@ -0,0 +1,4 @@
+#rowSums()
+#rowMeans() or colSums() or colMeans() can all be used like apply() but are more specialized
+
+
